3051609edbf25eb1cebaabff032f49351f13d4cd
Dennis Hermsmeier <d.hermsmeier@mittwald.de>
2019-04-30 16:44:12 +0200

    logging instead of printing

--- helper/gitconfig.go
+++ helper/gitconfig.go
@@ -1,28 +1,28 @@
 package helper
 
 import (
-	"fmt"
+	"github.com/mittwald/protodep/logger"
 	"gopkg.in/src-d/go-git.v4/plumbing/format/config"
 	"os"
 )
 
-func GitConfig(target string) string{
+func GitConfig(target string) string {
 	cfg := config.New()
 
 	home, err := os.UserHomeDir()
-	
-	r, err := os.Open(home+"/.gitconfig")
-	if err != nil{
-		fmt.Println(err)
+
+	r, err := os.Open(home + "/.gitconfig")
+	if err != nil {
+		logger.Error("%v", err)
 	}
 	decoder := config.NewDecoder(r)
 	err = decoder.Decode(cfg)
-	if err != nil{
-		fmt.Println(err)
+	if err != nil {
+		logger.Error("%v", err)
 	}
 
-	for _, subsec := range cfg.Section("url").Subsections{
-		if subsec.Options.Get("insteadOf") == target{
+	for _, subsec := range cfg.Section("url").Subsections {
+		if subsec.Options.Get("insteadOf") == target {
 			return subsec.Name
 		}
 	}
